977cfa4456e417fe93e026b83ead53408e55da75
root <soulww@163.com>
2023-12-28 09:49:39 +0800

    update crud example

--- examples/crud/src/main.rs
+++ examples/crud/src/main.rs
@@ -1,9 +1,9 @@
 use luna_orm::prelude::*;
 use luna_orm::LunaOrmResult;
 
-#[derive(Entity, Clone, Debug)]
+#[derive(Schema, Clone, Debug)]
 #[TableName = "user"]
-pub struct HelloEntity {
+pub struct UserEntity {
     #[PrimaryKey]
     id: i32,
     name: String,
@@ -19,17 +19,45 @@ async fn main() -> LunaOrmResult<()> {
 
     let mut db: DB<SqliteDatabase> = SqliteDatabase::build(config).await.unwrap().into();
 
+    let result = db.execute_plain("DROP TABLE IF EXISTS `user`").await?;
     let result = db.execute_plain(
         "CREATE TABLE IF NOT EXISTS `user`(`id` INT PRIMARY KEY, `age` INT, `name` VARCHAR(64))",
     ).await?;
 
-    let entity = HelloEntity {
+    // 1. insert entity
+    let entity = UserEntity {
         id: 1,
         name: "Allen".to_string(),
         age: Some(23),
     };
-
     let result = db.insert(&entity).await?;
+
+    // 2. update
+    let mutation = UserMutation {
+        name: None,
+        age: Some(24),
+    };
+    let primary = UserPrimary { id: 1 };
+    let result = db.update(&mutation, &primary).await?;
+
+    // 3. select
+    let selection = UserSelection {
+        id: false,
+        name: true,
+        age: true,
+    };
+    let entity: Option<UserSelectedEntity> = db.select(&primary, &selection).await?;
+    let expect_entity = UserSelectedEntity {
+        id: None,
+        name: Some("Allen".to_string()),
+        age: Some(24),
+    };
+    assert_eq!(entity, Some(expect_entity));
+
+    // 4. delete
+    let _ = db.delete(&primary).await?;
+    let entity: Option<UserSelectedEntity> = db.select(&primary, &selection).await?;
+    assert_eq!(entity, None);
 
     println!("result: {}", result);
     Ok(())
