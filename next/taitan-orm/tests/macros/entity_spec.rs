c7b5d0b18198d6a53e37c8200a887461fe3a42dc
汪蔚 <wangwei@wangweideMac-mini.local>
2024-12-18 16:29:30 +0800

    update macro test

--- next/taitan-orm/tests/macros/entity_spec.rs
+++ next/taitan-orm/tests/macros/entity_spec.rs
@@ -1,20 +1,30 @@
 
 use sqlx::sqlx_macros;
+use time::PrimitiveDateTime;
+use uuid::Uuid;
 use taitan_orm_macro::Schema;
 
 #[derive(Schema, Clone, Debug)]
 #[TableName = "user"]
 pub struct UserEntity {
-    age: Option<i32>,
-
     #[PrimaryKey]
     #[AutoIncrement]
     id: Option<i64>,
 
+    pub request_id: Uuid,
+
+    age: Option<i32>,
+    
     name: String,
+
+    pub birthday: Option<PrimitiveDateTime>,
 }
 
 #[sqlx_macros::test]
 pub async fn entity_macro_spec() -> taitan_orm::Result<()> {
+
+
+
+
     Ok(())
 }
