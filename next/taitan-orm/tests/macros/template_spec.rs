69dc981c582eca1251f3c84bca85b151ba4426ad
汪蔚 <wangwei@wangweideMac-mini.local>
2024-12-23 20:20:00 +0800

    update template test

--- next/taitan-orm/tests/macros/template_spec.rs
+++ next/taitan-orm/tests/macros/template_spec.rs
@@ -4,7 +4,7 @@ use taitan_orm::Schema;
 use taitan_orm_macro::TemplateRecord;
 use time::PrimitiveDateTime;
 use uuid::Uuid;
-
+use taitan_orm_trait::TemplateRecord;
 
 #[derive(TemplateRecord, Clone, Debug)]
 #[TemplateSql = "select * from ${name}"]
@@ -38,7 +38,43 @@ pub struct TestTemplate3<'a> {
     age: i32
 }
 
+
+#[derive(TemplateRecord, Clone, Debug)]
+#[TemplateSql = "select * from ${name} #{age} 'hello' "]
+pub struct TestTemplate4<'a> {
+    name: Cow<'a, str>,
+    age: i32
+}
+
+#[derive(TemplateRecord, Clone, Debug)]
+#[TemplateSql = "select * from ${name} #{age} \"hello ${name}\" "]
+pub struct TestTemplate5<'a> {
+    name: Cow<'a, str>,
+    age: i32
+}
+
 #[sqlx_macros::test]
 pub async fn template_macro_spec() -> taitan_orm::Result<()> {
+    let template = TestTemplate1 { name: Cow::Borrowed("wang") };
+    let sql = template.get_sql(None);
+    assert_eq!(sql, "select * from wang");
+
+    let template = TestTemplate2 { name: String::from("wang") };
+    let sql = template.get_sql(None);
+    assert_eq!(sql, "select * from ?");
+
+    let template = TestTemplate3 { name: Cow::Borrowed("wang"), age: 23 };
+    let sql = template.get_sql(None);
+    assert_eq!(sql, "select * from wang ?");
+
+
+    let template = TestTemplate4 { name: Cow::Borrowed("wang"), age: 23 };
+    let sql = template.get_sql(None);
+    assert_eq!(sql, "select * from wang ? 'hello'");
+
+    let template = TestTemplate5 { name: Cow::Borrowed("wang"), age: 23 };
+    let sql = template.get_sql(None);
+    assert_eq!(sql, "select * from wang ? \"hello ${name}\"");
+
     Ok(())
 }
